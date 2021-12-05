6e3e32d647b6f7b39db4d01f9886cb73d4b17ade
Constance <56352877+Constance-G@users.noreply.github.com>
2021-12-05 10:43:57 -0330

    loadlevel not complete
    Error trying to add CPatrol to entity

--- src/Scene_Zelda.cpp
+++ src/Scene_Zelda.cpp
@@ -5,10 +5,10 @@
 //  Year / Term:     2021FALL
 //  File Name:       Scene_Zelda.cpp
 // 
-//  Student Name:    Mahdi Rabie
-//  Student User:    mr1826
-//  Student Email:   mr1826@mun.ca
-//  Student ID:      201025756
+//  Student Name:    Mahdi Rabie, Constance Gray
+//  Student User:    mr1826, cdg165
+//  Student Email:   mr1826@mun.ca, cdg165@mun.ca
+//  Student ID:      201025756, 201050531
 //  Group Member(s): [enter student name(s)]
 //
 ///\/\\////\///////\////\\\\\\/\\/\/\\////\///////\////\\\\\\/\\
@@ -34,17 +34,93 @@ void Scene_Zelda::init(const std::string& levelPath)
     // STUDENT TODO:
     // Register the actions required to play the game
 
-    registerAction(sf::Keyboard::Escape, "QUIT");
-    registerAction(sf::Keyboard::P, "PAUSE");
-    registerAction(sf::Keyboard::Y, "TOGGLE_FOLLOW");      // Toggle follow camera
-    registerAction(sf::Keyboard::T, "TOGGLE_TEXTURE");      // Toggle drawing (T)extures
-    registerAction(sf::Keyboard::C, "TOGGLE_COLLISION");    // Toggle drawing (C)ollision Boxes
+    registerAction(sf::Keyboard::Escape, "QUIT");                       //  Exit to menu if in game and exit window if in menu
+    registerAction(sf::Keyboard::P, "PAUSE");                               //  Pause the game
+    registerAction(sf::Keyboard::Y, "TOGGLE_FOLLOW");          //  Toggle follow camera
+    registerAction(sf::Keyboard::T, "TOGGLE_TEXTURE");         //  Toggle drawing (T)extures
+    registerAction(sf::Keyboard::C, "TOGGLE_COLLISION");      //  Toggle drawing (C)ollision Boxes
+	registerAction ( sf::Keyboard::W, "UP" );                                //  Go Up/Jumping
+	registerAction ( sf::Keyboard::A, "LEFT" );                              //  Go Left
+	registerAction ( sf::Keyboard::S, "DOWN" );                          //  Go Down
+    registerAction ( sf::Keyboard::D, "RIGHT" );                          //   Go Right
+    registerAction ( sf::Keyboard::Space, "ATTACK" );                //   Player is attacking
 }
 
+//  Read in the level file and assign the assets to their starting positions
 void Scene_Zelda::loadLevel(const std::string& filename)
 {
+    // Reset the entity manager every time we load a level
     m_entityManager = EntityManager();
+	//  Prepare to read in the level file
+	std::ifstream fin ( filename );
+	std::string line;
 
+	//  Read in the level file and assign the attributes
+	while ( std::getline ( fin, line ) )
+	{
+		std::istringstream iss ( line );
+		std::string firstWord;
+		std::string animName, behaviourName;
+		int posX, posY, roomX, roomY, blockMove, blockVis, maxHealth, damage, numPatrolSpots, patrolPosX , patrolPosY;
+        float patrolSpeed, followSpeed;
+
+		iss >> firstWord;
+
+		if ( firstWord == "Player" )
+		{
+            iss >> m_playerConfig.X >> m_playerConfig.Y >>
+			           m_playerConfig.CX >> m_playerConfig.CY >>
+				       m_playerConfig.SPEED >> m_playerConfig.HEALTH;
+        }
+
+		else if ( firstWord == "Tile" )
+		{  
+			iss >> animName >> roomX >> roomY>>posX>>posY>>blockMove>>blockVis;
+			auto e = m_entityManager.addEntity ( "tile" );
+            e->addComponent<CAnimation> ( m_game->assets ().getAnimation ( animName ), true );
+			e->addComponent<CTransform> ( getPosition (roomX, roomY, posX, posY) );			
+			e->addComponent<CBoundingBox> ( (m_game->assets ().getAnimation ( animName ).getSize ()), blockMove, blockVis );            
+		}
+
+		else if ( firstWord == "NPC" )
+		{
+            iss >> animName >> roomX >> roomY >> posX >> posY >> blockMove >> blockVis>>
+                maxHealth>>damage>>behaviourName>>patrolSpeed;
+            //  Calculate the entity's center point
+            Vec2 calcPosition = getPosition ( roomX, roomY, posX, posY );
+            //  Create the new tile entity
+            auto e = m_entityManager.addEntity ( "npc" );
+            e->addComponent<CAnimation> ( m_game->assets ().getAnimation ( animName ), true );
+            e->addComponent<CTransform> (calcPosition);
+            e->addComponent<CBoundingBox> ( ( m_game->assets ().getAnimation ( animName ).getSize () ), blockMove, blockVis );
+            e->addComponent<CHealth>(maxHealth, maxHealth);
+            e->addComponent<CDamage>(damage);
+            //  Store the NPC's position coordinates based on behaviour type
+            if (behaviourName == "Follow" )
+            {
+                e->addComponent<CFollowPlayer>(calcPosition, patrolSpeed);
+                // Get the next entity
+               break;
+            }
+            //  If the npc patrols get the rest of the patrol coordinates
+            if ( behaviourName  == "Patrol" )
+            {
+                iss>>numPatrolSpots;
+                //  Read in the positions of patrol spots
+                for (int i = 0;  i < numPatrolSpots; i++)
+                {
+                    //  Store the patrol positions
+                    iss>>patrolPosX>>patrolPosY;
+                    Vec2 patrolPos = getPosition ( roomX, roomY, patrolPosX, patrolPosY);
+                    e->addComponent<CPatrol>(patrolPos, patrolSpeed);
+                }
+            }
+		}
+		else
+		{
+			std::cout << "Error in file reading";
+		}
+	}
     // STUDENT TODO:
     // Load the level file and put all entities in the manager
     // Use the getPosition() function below to convert room-tile coords to game world coords
@@ -52,16 +128,19 @@ void Scene_Zelda::loadLevel(const std::string& filename)
     spawnPlayer();
 }
 
+// This function takes in a room position and a grid position and 
+//  Returns a Vec2 indicating where the CENTER position of the Entity is
 Vec2 Scene_Zelda::getPosition(int rx, int ry, int tx, int ty) const
 {
-    // STUDENT TODO:
-    // Implement this function, which takes in the room (rx, ry) coordinate
-    // as well as the tile (tx, ty) coordinate, and returns the Vec2 game world 
-    // position of the center of the entitiy. 
-    //
-    // All tiles are of size 64x64 for this assignment
+    //  Convert supplied coordinates to pixels
+    float roomX = rx*20*64;
+    float roomY = ry*12*64;
+    float tileX     = (tx*64)-32 ;
+    float tileY = ( ty * 64 ) - 32;
+    auto posX = roomX+tileX ;
+    auto posY = roomY + tileY;
 
-    return Vec2(0, 0);
+    return Vec2(posX, posY);
 }
                               
 void Scene_Zelda::spawnPlayer()
@@ -71,6 +150,8 @@ void Scene_Zelda::spawnPlayer()
     m_player->addComponent<CAnimation>(m_game->assets().getAnimation("StandDown"), true);
     m_player->addComponent<CBoundingBox>(Vec2(48, 48), true, false);
     m_player->addComponent<CHealth>(7, 3);
+    //  Add the invincibility component to the player, set to false
+    m_player->addComponent<CInvincibility>(0);
 
     // STUDENT TODO:
     // Implement this function so that it uses the parameters input from the level file
@@ -90,49 +171,165 @@ void Scene_Zelda::spawnSword(std::shared_ptr<Entity> entity)
 
 void Scene_Zelda::update()
 {
-    m_entityManager.update();
+    //  Check if the game is paused
+    if (m_paused)
+    {
+        //  Game is currently paused, no systems updating
+    }
+    else
+    {
+		//  Update all entities
+		m_entityManager.update ();
+		//  Update all systems
+		sAI ();
+		sMovement ();
+		sStatus ();
+		sCollision ();
+		sAnimation ();
+		sCamera ();
 
-    // STUDENT TODO:
-    // Implement pause functionality
+		m_currentFrame++;
+    }
 
-    sAI();
-    sMovement();
-    sStatus();
-    sCollision();
-    sAnimation();
-    sCamera();
-
-    m_currentFrame++;
 }
 
 void Scene_Zelda::sMovement()
 {
-    // STUDENT TODO:
-    // 
-    // Implement all player movement functionality here based on 
-    // the player's input component variables
+    //  Store some attributes about the player
+	auto& pTransform = m_player->getComponent<CTransform> ();               //  Get a reference to the players Transform attributes
+	auto& pInput = m_player->getComponent<CInput> ();                                //  Get a reference to the players Input attributes
+	auto& playerV1 = m_player->getComponent<CTransform> ().velocity;    //  Get a reference to the players velocity
+	auto    playerV2 = Vec2 ( 0.0f, 0.0f );                                                                //  A new variable to hold the players updated velocity
+	auto& state = m_player->getComponent<CState> ().state;                        //  Get a reference to the players State attributes
+	
+
+	//  Store previous position before updating
+	pTransform.prevPos = pTransform.pos;
+
+	//  Update player movement based on input
+	if ( pInput.right )
+	{
+        //  Update the direction the player is facing
+		m_player->getComponent<CTransform> ().facing = Vec2 ( 1.0f, 1.0f );
+        //  Update the players animation
+		if ( state == "StandRight" ) { state = "RunRight"; }
+        //  Update the players velocity
+		playerV2.x += m_playerConfig.SPEED;
+	}
+	if ( pInput.left )
+	{
+		//  See pInput.right for comments about each lines intention
+		m_player->getComponent<CTransform> ().facing = Vec2 ( -1.0f, 1.0f );
+		if ( state == "StandRight" ) { state = "RunRIght"; }
+		playerV2.x -= m_playerConfig.SPEED;
+	}
+	if ( pInput.up )
+	{
+        //  See pInput.right for comments about each lines intention
+		m_player->getComponent<CTransform> ().facing = Vec2 ( 0.0f,- 1.0f );
+		if ( state == "StandUp" ) { state = "RunUp"; }
+		playerV2.y -= m_playerConfig.SPEED;        
+	}
+	if ( pInput.down )
+	{
+		//  See pInput.right for comments about each lines intention
+		m_player->getComponent<CTransform> ().facing = Vec2 ( 0.0f, 1.0f );
+		if ( state == "StandDown " ) { state = "RunDown"; }
+		playerV2.y += m_playerConfig.SPEED;
+	}
+
+	pTransform.velocity = playerV2;
+	pTransform.pos += playerV2;
+    /*  TODO: OLD CODE FROM A3 - Constance
+	//  Move the bullets on the map
+	for ( auto e : m_entityManager.getEntities ( "bullet" ) )
+	{
+		auto& entityX = e->getComponent<CTransform> ();
+		//  Check the direction of travel
+		if ( entityX.scale.x > 0 ) {
+			//  Bullet is traveling right
+			entityX.pos.x += entityX.velocity.x;
+		}
+		else {
+			//  Bullet is traveling left
+			entityX.pos.x -= entityX.velocity.x;
+		}
+	}*/
 }
 
+// Set the appropriate attributes to initialize the registered player input
 void Scene_Zelda::sDoAction(const Action& action)
 {
-    // STUDENT TODO:
-    //
-    // Implement all actions described for the game here
-    // Only the setting of the player's input component variables should be set here
-    // Do minimal logic in this function
+    //  Get a quick reference to the players state and input attributes
+	auto& state = m_player->getComponent<CState> ().state;
+	auto& playerInput = m_player->getComponent<CInput> ();
 
+    //  At the start of the action, get the requested action
     if (action.type() == "START")
     {
-        
-             if (action.name() == "PAUSE") { setPaused(!m_paused); }
-        else if (action.name() == "QUIT") { onEnd(); }
-        else if (action.name() == "TOGGLE_FOLLOW") { m_follow = !m_follow; }
-        else if (action.name() == "TOGGLE_TEXTURE") { m_drawTextures = !m_drawTextures; }
-        else if (action.name() == "TOGGLE_COLLISION") { m_drawCollision = !m_drawCollision; }
-    }
-    else if (action.type() == "END")
-    {
+        //  Game settings
+                if (action.name() == "PAUSE")                           { setPaused(!m_paused); }
+        else if (action.name() == "QUIT")                             { onEnd(); }
+        else if (action.name() == "TOGGLE_FOLLOW")      { m_follow = !m_follow; }
+        else if (action.name() == "TOGGLE_TEXTURE")     { m_drawTextures = !m_drawTextures; }
+        else if (action.name() == "TOGGLE_COLLISION")  { m_drawCollision = !m_drawCollision; }
 
+		//  Player movement inputs
+		//  These actions will not be performed while the game is paused
+		if ( !m_paused )
+		{
+			if ( action.name () == "RIGHT" )
+			{
+				playerInput.right = true;
+			}
+			else if ( action.name () == "LEFT" )
+			{
+				playerInput.left = true;
+			}
+			else if ( action.name () == "UP" )
+			{
+				playerInput.up = true;
+			}
+			else if ( action.name () == "DOWN" )
+			{
+                playerInput.down = true;
+			}
+            else if (action.name() == "ATTACK" )
+            {
+                playerInput.attack = true;
+            }
+        }
+        if ( !m_paused ) {
+	        //  The Action has ended
+	        if ( action.type () == "END" )
+	        {
+		        if ( action.name () == "RIGHT" )
+		        {
+			        playerInput.right = false;
+			        if ( state == "RunRight" ) { state = "StandRight"; }
+		        }
+		        else if ( action.name () == "LEFT" )
+		        {
+			        playerInput.left = false;
+                    if ( state == "RunRight" ) { state = "StandRight"; }
+		        }
+		        else if ( action.name () == "UP" )
+		        {
+			        playerInput.up = false;
+                    if ( state == "RunUp" ) { state = "StandUp"; }
+		        }
+
+		        else if ( action.name () == "DOWN" )
+		        {
+                    playerInput.down = false;
+                    if ( state == "RunDown" ) { state = "StandDown"; }
+		        }
+				else if ( action.name () == "ATTACK" )
+				{
+					playerInput.attack = true;
+				}
+	        }
+	    }
     }
 }
 
